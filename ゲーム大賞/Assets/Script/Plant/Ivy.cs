d5ee08a9bd94c478a10f4f07f9d6cd7e2904e15f
Kawamura1237 <rk1237@icloud.com>
2021-05-09 22:38:36 +0900

    スクリプトの追加 plant->種を生成する。フックショット的な動作は難しすぎて断念 seed->Ivy(蔦)を生成する。 Ivy->seedから生成あと蔦の長さ(Ivylong)の長さだけ上方向に蔦を伸ばす。

---
+++ ゲーム大賞/Assets/Script/Plant/Ivy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ivy : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    public float Ivelong = 50.0f;
+    void Start()
+    {
+        StartCoroutine("extend");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator extend()
+    {
+        for (int i = 0; i < Ivelong; i++)
+        {
+            transform.localScale += new Vector3(0, -0.01f, 0);
+            yield return new WaitForSeconds(0.01F);
+        }
+    }
+}
